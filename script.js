e147d2459c936a0f968c00e33d62e552c60816b5
Karmatack <kennedy31agost@gmail.com>
2024-07-22 17:35:14 -0500

    Actualizar archivos del repositorio

--- script.js
+++ script.js
@@ -1,76 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-<<<<<<< HEAD
 
-=======
->>>>>>> 1e5d27ceb4d9b986fca9e741b8d2319db7006fdf
     const encript_Btn = document.getElementById('encript-btn');
     const desencript_Btn = document.getElementById('desencript-btn');
     const salidasDiv = document.getElementById('Salidas');
 
-<<<<<<< HEAD
     mostrarImagenPorDefecto();
 
     encript_Btn.addEventListener('click', function() {
         const textoEntrada = document.querySelector('#Entradas textarea').value;
         const textoEncriptado = encriptarTexto(textoEntrada);
-=======
-    encript_Btn.addEventListener('click', function() {
-
-        const textoEntrada = document.querySelector('#Entradas textarea').value;
-
-        const textoEncriptado = encriptarTexto(textoEntrada);
-
->>>>>>> 1e5d27ceb4d9b986fca9e741b8d2319db7006fdf
         mostrarTextoEncriptado(textoEncriptado);
     });
 
     desencript_Btn.addEventListener('click', function() {
-<<<<<<< HEAD
         const textoEntrada = document.querySelector('#Entradas textarea').value;
         const textoDesencriptado = desencriptarTexto(textoEntrada);
-=======
-
-        const textoEntrada = document.querySelector('#Entradas textarea').value;
-
-        const textoDesencriptado = desencriptarTexto(textoEntrada);
-
->>>>>>> 1e5d27ceb4d9b986fca9e741b8d2319db7006fdf
         mostrarTextoEncriptado(textoDesencriptado);
     });
 
     function mostrarTextoEncriptado(texto) {
-<<<<<<< HEAD
         salidasDiv.innerHTML = '';
-=======
-
-        salidasDiv.innerHTML = '';
-
->>>>>>> 1e5d27ceb4d9b986fca9e741b8d2319db7006fdf
         if (texto.trim() !== '') {
             const textarea = document.createElement('textarea');
             textarea.textContent = texto;
             textarea.readOnly = true;
             salidasDiv.appendChild(textarea);
-<<<<<<< HEAD
 
             const botonCopiar = document.createElement('button');
             botonCopiar.textContent = 'Copiar';
             botonCopiar.classList.add('custom-button');
-=======
-        
-            const botonCopiar = document.createElement('button');
-            const imagenCopiar = document.createElement('img');
-            imagenCopiar.src = './imagenes/btn-cp.png'; 
-            imagenCopiar.alt = 'Copiar';
-            botonCopiar.appendChild(imagenCopiar);
->>>>>>> 1e5d27ceb4d9b986fca9e741b8d2319db7006fdf
             botonCopiar.addEventListener('click', function() {
                 textarea.select();
                 document.execCommand('copy');
             });
             salidasDiv.appendChild(botonCopiar);
         } else {
-<<<<<<< HEAD
             mostrarImagenPorDefecto();
         }
     }
@@ -92,7 +56,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function encriptarTexto(texto) {
-
         const reglas = {
             'e': 'enter',
             'i': 'imes',
@@ -114,7 +77,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function desencriptarTexto(textoEncriptado) {
-        
         const reglas = {
             'enter': 'e',
             'imes': 'i',
@@ -124,7 +86,6 @@ document.addEventListener('DOMContentLoaded', function() {
         };
 
         let textoDesencriptado = '';
-        
         const palabras = textoEncriptado.split(' ');
         palabras.forEach((palabra, index) => {
             if (index > 0) textoDesencriptado += ' '; // Reponemos los espacios
@@ -148,25 +109,5 @@ document.addEventListener('DOMContentLoaded', function() {
             textoDesencriptado += palabraDesencriptada;
         });
         return textoDesencriptado;
-=======
-
-            const imagen = document.createElement('img');
-            imagen.src = './imagenes/Ningun_elemento.png';
-            imagen.alt = 'Ningún elemento';
-
-            salidasDiv.appendChild(imagen);
-            
-        }
-    }
-
-    function encriptarTexto(texto) {
-
-        return btoa(texto);
-    }
-
-    function desencriptarTexto(textoEncriptado) {
-
-        return atob(textoEncriptado);
->>>>>>> 1e5d27ceb4d9b986fca9e741b8d2319db7006fdf
     }
 });
